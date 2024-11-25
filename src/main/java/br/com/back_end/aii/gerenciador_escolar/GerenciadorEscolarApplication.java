29888121a910ebc04794007c4f5bfa060c23784f
Gentil Silva <silvagentil.sg@gmail.com>
2024-11-24 23:18:33 -0300

    Init setup

--- src/main/java/br/com/back_end/aii/gerenciador_escolar/GerenciadorEscolarApplication.java
+++ src/main/java/br/com/back_end/aii/gerenciador_escolar/GerenciadorEscolarApplication.java
@@ -1,13 +1,13 @@
-package br.com.back_end_aai.GeranciadorEscolar;
+package br.com.back_end.aii.gerenciador_escolar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeranciadorEscolarApplication {
+public class GerenciadorEscolarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeranciadorEscolarApplication.class, args);
+		SpringApplication.run(GerenciadorEscolarApplication.class, args);
 	}
 
 }
--- src/test/java/br/com/back_end/aii/gerenciador_escolar/GerenciadorEscolarApplicationTests.java
+++ src/test/java/br/com/back_end/aii/gerenciador_escolar/GerenciadorEscolarApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.back_end_aai.GeranciadorEscolar;
+package br.com.back_end.aii.gerenciador_escolar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GeranciadorEscolarApplicationTests {
+class GerenciadorEscolarApplicationTests {
 
 	@Test
 	void contextLoads() {
